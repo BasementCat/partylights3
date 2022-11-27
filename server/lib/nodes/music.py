e2aa73ab1c94a533e696e963a26d3e0489aff269
Alec Elton <alec.elton@gmail.com>
2022-11-27 10:46:54 -0600

    stuff

---
+++ server/lib/nodes/music.py
@@ -0,0 +1,12 @@
+from . import Node, NodeIO
+
+
+# class PrintNode(Node):
+#     NAME = 'PrintNode'
+#     DESCRIPTION = "Prints the data sent to its input"
+#     INPUTS = [NodeIO('data', str)]
+
+#     def process(self, from_node, output_name, input_name, value, input_cache, output_cache):
+#         print(f"From {from_node.name}: {output_name}: '{value}'")
+#         return {}, False
+
